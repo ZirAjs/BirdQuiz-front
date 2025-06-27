097b9a3f6389124918555adce3a108543caf0cff
zirajs <zirajs7@gmail.com>
2025-06-27 17:17:09 +0900

    Fix embed

--- components/Embed.tsx
+++ components/Embed.tsx
@@ -1,16 +1,30 @@
 import { useEffect, useRef, useState } from "react";
-import { Platform } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
 import { ThemedText } from "./ThemedText";
 import { ThemedView } from "./ThemedView";
 
 interface Props {
   src: string;
-  styles: any;
+  hidden: boolean;
 }
 
-export function EmbedHtml({ src, styles }: Props) {
+const defaultStyles = StyleSheet.create({
+  embedStyle: {
+    width: 290,
+    height: 200,
+  },
+  hiddenStyle: {
+    width: 0,
+    height: 0,
+  },
+});
+
+export function EmbedHtml({ src, hidden }: Props) {
   const [loading, setLoading] = useState(true);
+  const [styles, setStyles] = useState<any>(
+    hidden ? defaultStyles.hiddenStyle : defaultStyles.embedStyle
+  );
 
   const webViewRef = useRef<WebView>(null);
 
@@ -23,11 +37,25 @@ export function EmbedHtml({ src, styles }: Props) {
     }
   }, [src]);
 
+  useEffect(() => {
+    if (hidden) {
+      setStyles(defaultStyles.hiddenStyle);
+    } else {
+      setStyles(defaultStyles.embedStyle);
+    }
+  }, [hidden]);
+
   return (
     <ThemedView>
-      {loading && <ThemedText>불러오는 중...</ThemedText>}
+      {/* {loading && <ThemedText>불러오는 중...</ThemedText>} */}
       {Platform.OS === "web" ? (
-        <iframe src={src} style={styles} frameBorder="0" allowFullScreen />
+        <iframe
+          src={src}
+          style={styles}
+          frameBorder="0"
+          allowFullScreen
+          hidden={hidden}
+        />
       ) : (
         <WebView
           ref={webViewRef}
